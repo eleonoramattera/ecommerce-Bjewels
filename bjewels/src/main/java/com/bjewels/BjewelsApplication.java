41063aeb8404eb0fdd25b1c1a0daaa0287b015b9
Eleonora Mattera <m.eleonora98@live.it>
2023-10-18 00:58:11 +0200

    first commit and pom

---
+++ bjewels/src/main/java/com/bjewels/BjewelsApplication.java
@@ -0,0 +1,13 @@
+package com.bjewels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BjewelsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BjewelsApplication.class, args);
+	}
+
+}
---
+++ bjewels/src/test/java/com/bjewels/BjewelsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bjewels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BjewelsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
